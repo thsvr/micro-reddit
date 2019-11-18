075a4207b361833da9d248f00b2237948901e639
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-11-18 17:33:28 -0500

    add Post table

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  validates :title, length: { in: 5..50 }, presence: true
+  validates :body, length: { minimum: 10 }, presence: true
+end
