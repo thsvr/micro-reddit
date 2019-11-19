cbc0cb7474398d9fc04a854dfad721397c0533e9
thsvr <vr.ths.zd@gmail.com>
2019-11-19 14:48:24 -0300

    add rubocop file and coorect some offenses automatically with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, length: { in: 3..20 }, presence: true
-    has_many :comments
-    has_many :posts
+  validates :username, length: { in: 3..20 }, presence: true
+  has_many :comments
+  has_many :posts
 end
